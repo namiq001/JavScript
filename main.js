33368db92387010ec1adc92e4b55c63193d3eacf
User <nammiq@example.com>
2023-03-10 01:08:39 +0400

    JavaScript

---
+++ main.js
@@ -0,0 +1,203 @@
+var div=document.getElementById("table")
+
+var table=document.createElement("table")
+
+var thead=document.createElement("thead")
+
+var th=document.createElement("th")
+
+var text = document.createTextNode("Name")
+
+th.appendChild(text)
+
+thead.appendChild(th)
+
+table.appendChild(thead)
+
+div.appendChild(table)
+
+var th=document.createElement("th")
+
+var text = document.createTextNode("Surname")
+
+th.appendChild(text)
+
+thead.appendChild(th)
+
+table.appendChild(thead)
+
+div.appendChild(table)
+
+var th=document.createElement("th")
+
+var text = document.createTextNode("Age")
+
+th.appendChild(text)
+
+thead.appendChild(th)
+
+table.appendChild(thead)
+
+div.appendChild(table)
+
+
+// tbody
+
+var tbody=document.createElement("tbody")
+
+var td=document.createElement("td")
+
+var text = document.createTextNode("Namiq")
+
+td.appendChild(text)
+
+tbody.appendChild(td)
+
+table.appendChild(tbody)
+
+div.appendChild(table)
+
+var td=document.createElement("td")
+
+var text = document.createTextNode("Kerimov")
+
+td.appendChild(text)
+
+tbody.appendChild(td)
+
+table.appendChild(tbody)
+
+div.appendChild(table)
+
+var td=document.createElement("td")
+
+var text = document.createTextNode("20")
+
+td.appendChild(text)
+
+tbody.appendChild(td)
+
+table.appendChild(tbody)
+
+div.appendChild(table)
+
+
+
+var tbody=document.createElement("tbody")
+
+var td=document.createElement("td")
+
+var text = document.createTextNode("Samiq")
+
+td.appendChild(text)
+
+tbody.appendChild(td)
+
+table.appendChild(tbody)
+
+div.appendChild(table)
+
+var td=document.createElement("td")
+
+var text = document.createTextNode("Veliyev")
+
+td.appendChild(text)
+
+tbody.appendChild(td)
+
+table.appendChild(tbody)
+
+div.appendChild(table)
+
+var td=document.createElement("td")
+
+var text = document.createTextNode("20")
+
+td.appendChild(text)
+
+tbody.appendChild(td)
+
+table.appendChild(tbody)
+
+div.appendChild(table)
+
+var tbody=document.createElement("tbody")
+
+var td=document.createElement("td")
+
+var text = document.createTextNode("Sahbaz")
+
+td.appendChild(text)
+
+tbody.appendChild(td)
+
+table.appendChild(tbody)
+
+div.appendChild(table)
+
+var td=document.createElement("td")
+
+var text = document.createTextNode("Verdialiyev")
+
+td.appendChild(text)
+
+tbody.appendChild(td)
+
+table.appendChild(tbody)
+
+div.appendChild(table)
+
+var td=document.createElement("td")
+
+var text = document.createTextNode("20")
+
+td.appendChild(text)
+
+tbody.appendChild(td)
+
+table.appendChild(tbody)
+
+div.appendChild(table)
+
+var tbody=document.createElement("tbody")
+
+var td=document.createElement("td")
+
+var text = document.createTextNode("Elvin")
+
+td.appendChild(text)
+
+tbody.appendChild(td)
+
+table.appendChild(tbody)
+
+div.appendChild(table)
+
+var td=document.createElement("td")
+
+var text = document.createTextNode("Nerimanov")
+
+td.appendChild(text)
+
+tbody.appendChild(td)
+
+table.appendChild(tbody)
+
+div.appendChild(table)
+
+var td=document.createElement("td")
+
+var text = document.createTextNode("20")
+
+td.appendChild(text)
+
+tbody.appendChild(td)
+
+table.appendChild(tbody)
+
+div.appendChild(table)
+
+
+
+
+
